a246018694a4480ab224f2f0b9d9dfd6c09cac32
jerekarppinen <jere.karppinen88@gmail.com>
2015-07-26 15:46:33 +0300

    test

--- XmlPluginTests.py
+++ XmlPluginTests.py
@@ -1,1 +1,23 @@
-XmlPluginTests.py
+import os
+import unittest
+from lxml import etree
+
+import ParsePaths
+
+class PathsXmlFileContainsCorrectFilePaths(unittest.TestCase):
+
+	def test_givenPathsXmlFileExists(self):
+		self.assertTrue(os.path.isfile("paths.xml"))
+
+	def test_thenDeploymentPomPathContainsPomXml(self):
+		tree = etree.parse("paths.xml")
+		element = tree.xpath("//paths/deployment/deploymentPomPath/text()")
+		self.assertEquals(element[0][-7:], "pom.xml")
+
+	def test_andArtifactXmlContainsArtifactXml(self):
+		tree = etree.parse("paths.xml")
+		element = tree.xpath("//paths/artifactXmls/artifactXml/text()")
+		self.assertEquals(element[0][-12:], "artifact.xml")
+
+if __name__ == '__main__':
+    unittest.main()
